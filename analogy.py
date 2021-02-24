8dcff109cfae7f71605430851ba2ce15bad02c57
PAM <pa.murena@gmail.com>
2021-02-24 09:50:07 +0200

    Refactor

---
+++ analogy.py
@@ -0,0 +1,98 @@
+import sys
+from utils import getTransformationPart1, getLengthInstruction, getPart2, applyTransformation, writeInstruction, getTransformation2
+
+
+
+def solveAnalogy(A, B, C):
+    """
+    Returns a solution (or a list of valid solutions) for 
+    the analogical equation "`A` : `B` # `C` : x", where 
+    each solution is constituted of the solution term D 
+    and the corresponding transformation.
+    """
+    
+    min_length_result = len(C) + len(B) - len(A)
+
+    final_result = []
+
+    result_transf_1 = []
+    result_varA = []
+    result_varC = []
+    list_varA = []
+    list_varC = []
+
+    getTransformationPart1("", A, C, list_varA, list_varC, result_transf_1, result_varA, result_varC)
+    min_length = sys.maxsize
+    
+    for x in range(len(result_transf_1)):
+        ll = getLengthInstruction(result_transf_1[x], result_varA[x] + result_varC[x])
+        if (ll <= min_length):
+            result_transf_2 = []
+            result_varB = []
+            l = result_varA[x]
+            getTransformation2(result_transf_1[x] + ",:", B, l, result_transf_2, result_varB)
+            for y in range(len(result_transf_2)):
+
+                ll = getLengthInstruction(result_transf_2[y], result_varB[y] + result_varC[x])
+                if (ll <= min_length):
+                    partInstruction_B = getPart2(result_transf_2[y])
+                    result_varD = list(result_varC[x])
+                    D = applyTransformation(partInstruction_B, result_varD)
+                    ll = getLengthInstruction(result_transf_2[y], result_varB[y] + result_varD)
+
+                    if (ll < min_length and len(D) >= min_length_result):
+                        min_length = ll
+                        final_result = [ [D, writeInstruction(result_transf_2[y], result_varB[y], result_varD)] ]
+                    elif (ll == min_length and len(D) >= min_length_result):
+                        final_result.append([D, writeInstruction(result_transf_2[y], result_varB[y], result_varD)])
+    return final_result, min_length
+
+
+
+
+
+
+def solveAnalogy_proba(A, B, C):
+    """
+    Returns a solution (or a list of valid solutions) for 
+    the analogical equation "`A` : `B` # `C` : x", where 
+    each solution is constituted of the solution term D 
+    and the corresponding transformation.
+    """
+    
+    possible_results = {}
+
+    result_transf_1 = []
+    result_varA = []
+    result_varC = []
+    list_varA = []
+    list_varC = []
+
+    getTransformationPart1("", A, C, list_varA, list_varC, result_transf_1, result_varA, result_varC)
+    min_length = sys.maxsize
+    
+    for x in range(len(result_transf_1)):
+        ll = getLengthInstruction(result_transf_1[x], result_varA[x] + result_varC[x])
+        if (ll <= min_length):
+            result_transf_2 = []
+            result_varB = []
+            l = result_varA[x]
+            getTransformation2(result_transf_1[x] + ",:", B, l, result_transf_2, result_varB)
+            for y in range(len(result_transf_2)):
+
+                ll = getLengthInstruction(result_transf_2[y], result_varB[y] + result_varC[x])
+                if (ll <= min_length):
+                    partInstruction_B = getPart2(result_transf_2[y])
+                    result_varD = list(result_varC[x])
+                    D = applyTransformation(partInstruction_B, result_varD)
+                    ll = getLengthInstruction(result_transf_2[y], result_varB[y] + result_varD)
+                    
+                    if D in possible_results:
+                        possible_results[D] += 2**(-ll)
+                    else: 
+                        possible_results[D] = 2 **(-ll)
+                        
+    # Normalization
+    factor = 1.0/sum(possible_results.values())
+    for D in possible_results: possible_results[D] *= factor
+    return dict(sorted(possible_results.items(), key=lambda item: item[1], reverse=True))
---
+++ retrieval.py
@@ -0,0 +1,128 @@
+from analogy import solveAnalogy
+from utils import getTransformationPart1, length_letter, getLengthInstruction, getTransformation2
+import sys
+
+
+def retrieval(CB, C, dist):
+    min_dist = sys.maxsize
+    retrieved_case = []
+    for ab in CB:
+        distance = dist(ab, C)
+        if distance == min_dist: retrieved_case.append(ab)
+        elif distance < min_dist: 
+            min_dist = distance
+            retrieved_case = [ab]
+    return retrieved_case, min_dist
+
+
+
+def dist1(ab,c):
+    """
+    d1(A:B,C) = min_D K(A:B::C:D)
+    """
+    a = ab[0]
+    b = ab[1]
+    _, dist = solveAnalogy(a,b,c)
+    return dist
+    
+
+def getK_A(a):
+    """
+    returns K(A)
+    """
+    result_transf_1 = []
+    result_varA = []
+
+    getTransformationPart1("", a, a, [], [], result_transf_1, result_varA, [])
+    
+    min_dist = len(a) * length_letter
+    
+    for x in range(len(result_transf_1)):
+        ll = getLengthInstruction(result_transf_1[x], result_varA[x], 1, 0, 0)
+        if ll < min_dist: 
+            min_dist = ll
+            #print(result_transf_1[x], result_varA[x])
+
+    return min_dist
+
+def dist2(ab,c):
+    """
+    d2(A:B,C) = min_D K(A:B::C:D) - K(A)
+    """
+    a = ab[0]
+    b = ab[1]
+    _, dist = solveAnalogy(a,b,c)
+    #return dist - len(a) * length_letter
+    return dist - getK_A(a)
+
+def dist3(ab,c):
+    """
+    d3(A:B,C) = min_D K(A:B::C:D) - K(A::B)
+    """
+    a = ab[0]
+    b = ab[1]
+    _, dist = solveAnalogy(a,b,c)
+    
+    # Compute K(A::B)
+    result_transf_1 = []
+    result_varA = []
+
+
+    getTransformationPart1("", a, a, [], [], result_transf_1, result_varA, [])
+    min_length = sys.maxsize
+    
+    for x in range(len(result_transf_1)):
+        ll = getLengthInstruction(result_transf_1[x], result_varA[x], 1, 1, 0)
+        if (ll <= min_length):
+            result_transf_2 = []
+            result_varB = []
+            l = result_varA[x]
+            getTransformation2(result_transf_1[x] + ",:", b, l, result_transf_2, result_varB)
+            #print(result_transf_2, result_varB)
+            for y in range(len(result_transf_2)):
+                ll = getLengthInstruction(result_transf_2[y], result_varB[y], 1, 1, 0)
+                if ll < min_length: 
+                    #print("--------")
+                    #print(result_transf_2[y])
+                    #print(result_varB[y])
+                    #print('--------')
+                    min_length = ll
+    
+    return dist - min_length
+
+
+def dist4(ab,c):
+    """
+    d4(A:B,C) = K(A::C)
+    """
+    a = ab[0]
+    
+    transformations = []
+    varA = []
+    varC = []
+
+    getTransformationPart1("", a, c, [], [], transformations, varA, varC)
+    
+    min_dist = sys.maxsize
+    
+    for x in range(len(transformations)):
+        ll = getLengthInstruction(transformations[x], varA[x] + varC[x], 2, 0, 1)
+        if ll < min_dist: 
+            min_dist = ll
+            # print(transformations[x])
+    return min_dist
+
+
+def dist5(ab,c):
+    """
+    d5(A:B,C) = K(A::C) - K(A)
+    """
+    #return dist4(ab,c) - len(ab[0]) * length_letter
+    return dist4(ab,c) - getK_A(ab[0])
+
+
+###############################################################################
+    
+def compare_distances(a,b,c):
+    ab=[a,b]
+    print(dist1(ab,c), dist2(ab,c), dist3(ab,c), dist4(ab,c), dist5(ab,c))
