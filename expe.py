9cc3ae8617bfac28b8298de2f06724d038bb86b5
PAM <pa.murena@gmail.com>
2021-05-01 21:21:06 +0300

    adding pipeline

--- expe.py
+++ expe.py
@@ -1,8 +1,10 @@
 from CBR import retrieval
-from CBR import analogy
 from CB_inference import adaptation, update_probas_2, proba_1nn_total, init
 import numpy as np
 import pandas as pd
+import random
+import seaborn as sns
+import time
 
 ###############################################################################
 
@@ -71,59 +73,13 @@ def toy():
     
 
 
+###############################################################################
 
 
-
-df = pd.read_csv ("./data/FI/Genitive/gen.txt")
-df = df[df.genitive != '—']
-df = df[df.genitive != '–']
-
-
-type2 = df[df.type == 2]
-type11 = df[df.type == 11]
-type38 = df[df.type == 38]
-type41 = df[df.type == 41]
-
-
-n_user = 2
-
-CB_user = pd.concat([type2.sample(n=n_user),
-                      type11.sample(n=n_user),
-                      type38.sample(n=n_user),
-                       type41.sample(n=n_user)])
-
-CB_user = CB_user.filter(items = ['nominative','genitive']).values.tolist()  
-
-
-n_teach = 5        
-CB_teach = pd.concat([type2.sample(n=n_teach),
-                      type11.sample(n=n_teach),
-                      type38.sample(n=n_teach),
-                      type41.sample(n=n_teach)])
-
-CB_teach = CB_teach.filter(items = ['nominative','genitive']).values.tolist()  
-        
-n_test = 3
-
-CB_test = pd.concat([type2.sample(n=n_test),
-                       type11.sample(n=n_test),
-                       type38.sample(n=n_test),
-                       type41.sample(n=n_test)])
-
-CB_test = CB_test.filter(items = ['nominative','genitive']).values.tolist()  
-
-def evaluate(CB_test, CB_user, distances_def, probas_cb, probas_dist, proba_harmony):
+def evaluate(X_s, Y_s, X_t, Y_t, a_solutions, a_distances, a_orders, CB_user, distances_def, probas_cb, probas_dist, proba_harmony):
     score = 0
     
-    X_s = [z[0] for z in CB_user]
-    Y_s = [z[1] for z in CB_user]
-    
-    X_t = [z[0] for z in CB_test]
-    Y_t = [z[1] for z in CB_test]
-    
-    a_solutions, a_distances, a_orders = init(X_s, Y_s, X_t, distances_def)
-    
-    for tgt in range(len(CB_test)):
+    for tgt in range(len(X_s)):
         y = Y_t[tgt]
         
         order = [a_orders[d][tgt] for d in range(len(distances_def))]
@@ -141,7 +97,7 @@ def evaluate(CB_test, CB_user, distances_def, probas_cb, probas_dist, proba_harm
                         else:
                             p += proba_harmony * probas_dist[d] * probas[d][v]
         score += p
-    return score / len(CB_test)
+    return score / len(X_s)
 
 
 def evaluate2(CB_test, CB_user, distances_def, probas_cb, probas_dist, proba_harmony):
@@ -175,16 +131,166 @@ def evaluate2(CB_test, CB_user, distances_def, probas_cb, probas_dist, proba_har
     return score / len(CB_test)
 
 
-# ------- Only for testing
+
+
+
+
+###############################################################################
+#### Pipeline
+
+# Data generation:
+
+start_time = time.time()
+
+df = pd.read_csv ("./data/FI/Genitive/gen.txt")
+df = df[df.genitive != '—']
+df = df[df.genitive != '–']
+
+
+type2 = df[df.type == 2]
+type11 = df[df.type == 11]
+type38 = df[df.type == 38]
+type41 = df[df.type == 41]
+
+
+# Parameters
+
 distances_def = [retrieval.dist2, retrieval.dist3, retrieval.dist5]
-probas_cb = .5 * np.ones(len(CB_user))
-probas_dist = np.ones(len(distances_def)) / len(distances_def)
-proba_harmony = .5
+
+
+# User definition:
+
+n_user = 5
+
+# CB contains all the possible cases that the user could know
+CB = pd.concat([type2.sample(n=n_user),
+                type11.sample(n=n_user),
+                type38.sample(n=n_user),
+                type41.sample(n=n_user)])
+
+CB = CB.filter(items = ['nominative','genitive']).values.tolist()  
+X = [z[0] for z in CB]
+Y = [z[1] for z in CB]
+n_words = len(CB)
+
+# TODO: Choice of indices
+known_indices = [0,5,10]
+
+# CB_user contains the cases that the user actually knows
+CB_user = [CB[i] for i in known_indices]
+
+X_user = [z[0] for z in CB_user]
+Y_user = [z[1] for z in CB_user]
+
+distance_user = distances_def[2]
+harmony_user = True
+
+
+# Initialization of the test:
+
+n_test = 2
+
+CB_test = pd.concat([type2.sample(n=n_test),
+                       type11.sample(n=n_test),
+                       type38.sample(n=n_test),
+                       type41.sample(n=n_test)])
+
+CB_test = CB_test.filter(items = ['nominative','genitive']).values.tolist()  
+
+
+X_test = [z[0] for z in CB_test]
+Y_test = [z[1] for z in CB_test]
+
+a_solutions_test, a_distances_test, a_orders_test = init(X_user, Y_user, X_test, distances_def)
+
+
+
+
+
+
+# Teaching
+
+
+
+
+n_teach = 5
+CB_teach = pd.concat([type2.sample(n=n_teach),
+                      type11.sample(n=n_teach),
+                      type38.sample(n=n_teach),
+                      type41.sample(n=n_teach)])
+
+CB_teach = CB_teach.filter(items = ['nominative','genitive']).values.tolist()
+n_words_teach = len(CB_teach)
+X_teach = [z[0] for z in CB_teach]
+dict_X = {X_teach[i]:i for i in range(len(X_teach))}
+
+a_solutions, a_distances, a_orders = init(X, Y, X_teach, distances_def)
+
+
+
+runs = []
+steps = []
+p_harmony = []
+p_d0 = []
+p_d1 = []
+p_d2 = []
+
+
+n_runs = 20
+
+for r in range(n_runs):
+    print(r)
+    
+    # Priors
+
+    probas_cb = .5 * np.ones(len(CB))
+    probas_dist = np.ones(len(distances_def)) / len(distances_def)
+    proba_harmony = .5
+    
+    
+    randomized = random.sample(list(range(n_words_teach)), n_words_teach)
+    runs.append(r)
+    steps.append(0)
+    p_harmony.append(proba_harmony)
+    p_d0.append(probas_dist[0])
+    p_d1.append(probas_dist[1])
+    p_d2.append(probas_dist[2])
+    
+    for i in range(n_words_teach):
+        #print("word", i)
+
+        
+        x = CB_teach[randomized[i]][0]
+        source, _ = retrieval.retrieval(CB_user, x, distance_user)
+        y = adaptation(source[0][0], source[0][1], x, harmony_user)
+        probas_cb, probas_dist, proba_harmony = update_probas_2(x, y, probas_cb, probas_dist, proba_harmony, X, Y, n_words, distances_def, dict_X, a_solutions, a_orders)
+        
+        runs.append(r)
+        steps.append(i+1)        
+        p_harmony.append(proba_harmony)
+        p_d0.append(probas_dist[0])
+        p_d1.append(probas_dist[1])
+        p_d2.append(probas_dist[2])
+
+
+# Plot
+
+
+print("--- %s seconds ---" % (time.time() - start_time))
+        
+df = pd.DataFrame(data={'run': runs, 'step': steps, 'p_d0': p_d0, 'p_d1': p_d1, 'p_d2': p_d2})
+
+sns.lineplot(x='step', y='value', hue='variable', 
+             data=pd.melt(df, id_vars = ['step', 'run']))
+
+
+
+# ------- Only for testing
+#distances_def = [retrieval.dist2, retrieval.dist3, retrieval.dist5]
+
 # -------
 
-import time
-start_time = time.time()
-# toy()
 
-print(evaluate(CB_test, CB_user, distances_def, probas_cb, probas_dist, proba_harmony))
-print("--- %s seconds ---" % (time.time() - start_time))
+#print("start")
+#print(evaluate(CB_test, CB_user, distances_def, probas_cb, probas_dist, proba_harmony))
+#print("--- %s seconds ---" % (time.time() - start_time))
