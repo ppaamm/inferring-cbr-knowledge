ea229ece292dc782afabd1d15db80018a8d873fd
Pierre-Alexandre Murena <pierre-alexandre.murena@aalto.fi>
2022-01-21 09:58:00 +0200

    creating dataloader

---
+++ data/dataloader.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from typing import List, Tuple
+
+
+class DataLoader:
+    
+    def __init__(self, path: str, case: str):
+        self.path = path
+        self.case = case
+        
+        # Loading data
+        df = pd.read_csv (self.path)
+        # Filtering out invalid data
+        self.df = df[df[self.case] != '—']
+
+
+    def generate_CB(self, n_data_per_type: List[Tuple[int, int]]):
+        sampled_df = [self.df[self.df.type == word_type[0]].sample(n=word_type[1]) for word_type in n_data_per_type]
+        CB = pd.concat(sampled_df).filter(items = ['nominative','inessive']).values.tolist()  
+        return CB
